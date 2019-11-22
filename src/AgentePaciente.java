8c91d1740e33c56bc7ca9fa24e41b6ccb7a58f08
Pedro Igor <pedroigor.silva@outlook.com>
2019-11-21 23:16:16 -0300

    Adicionando agente paciente.

---
+++ src/AgentePaciente.java
@@ -0,0 +1,222 @@
+import jade.core.*;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.lang.acl.MessageTemplate;
+
+public class AgentePaciente extends Agent{
+	
+	private static final long serialVersionUID = -216671578848886786L;
+	private AID[] AgentesMedicos;
+	class makingFriendsBehaviour extends CyclicBehaviour {
+		private static final long serialVersionUID = 776821786531384442L;
+
+		private String nome;
+		private Integer idade;
+		private String areaDeAtendimento;
+
+
+		public String getNome() {
+			return nome;
+		}
+
+		public void setNome(String nome) {
+			this.nome = nome;
+		}
+
+
+		public String getAreaDeAtendimento() {
+			return areaDeAtendimento;
+		}
+
+		public void setAreaDeAtendimento(String areaDeAtendimento) {
+			this.areaDeAtendimento = areaDeAtendimento;
+		}
+
+
+		
+		private int n = 0;
+		
+		public makingFriendsBehaviour(Agent a) {
+			super(a);
+		}
+
+		@Override
+		public void action() {
+			System.out.println("Inicializando a action do meu comportamento!");
+			
+			System.out.println("Recebendo o conteúdo enviado por um terceiro!");
+			ACLMessage msg = myAgent.receive();
+			if(msg != null) {
+				System.out.println("Criando uma resposta!");
+				ACLMessage reply = msg.createReply();
+				
+				if(msg.getPerformative() == ACLMessage.REQUEST) {
+					System.out.println("REQUEST!");
+					String content = msg.getContent();
+					System.out.println(++n + " " + getLocalName() + ": recebi " + content);
+					
+					if(content != null) {
+						System.out.println("INFORM!");
+						reply.setPerformative(ACLMessage.INFORM);
+						reply.setContent("Oi! Claro! Já sou seu amigo!");
+						System.out.println("Enviando msg: " + reply.getContent());
+					} else {
+						System.out.println("REFUSE!");
+						reply.setPerformative(ACLMessage.REFUSE);
+						reply.setContent("No momento, estou triste e não quero ter amigo");
+						System.out.println("Enviando msg: " + reply.getContent());
+					}
+				} else {
+					System.out.println("NOT_UNDERSTOOD!");
+					reply.setPerformative(ACLMessage.NOT_UNDERSTOOD);
+					reply.setContent("Não entendi sua mensagem.");
+					System.out.println("Enviando msg: " + reply.getContent());
+				}
+				
+				System.out.println("Msg enviada!");
+				send(reply);
+			}  else {
+				block();
+			}
+		}
+		
+	}
+	private String areaDeAtendimento;
+	protected void setup() {
+
+		System.out.println("Olá agente "+getName()+". Precisa de atendimento em que área?");
+		Object[] args = getArguments();
+		if(args != null && args.length >0) {
+			areaDeAtendimento = (String) args[0];
+			System.out.println("Estou procurando por atendimento em " + areaDeAtendimento);
+
+			addBehaviour(new TickerBehaviour(this, 60000) {
+				@Override
+				protected void onTick() {
+					System.out.println("Olá médicos, preciso de atendimento em " + areaDeAtendimento);
+					DFAgentDescription template = new DFAgentDescription();
+					template.setName(getAID());
+					ServiceDescription sd = new ServiceDescription();
+					sd.setName(getName());
+					sd.setType("Atendimento Médico");
+					template.addServices(sd);
+					try {
+						DFAgentDescription[] result = DFService.search(myAgent, template);
+						System.out.println("Os seguintes médicos estão disponveís: ");
+						AgentesMedicos = new AID[result.length];
+						for (int i = 0; i < result.length; ++i) {
+							AgentesMedicos[i] = result[i].getName();
+							System.out.println(AgentesMedicos[i].getName());
+						}
+
+					} catch (FIPAException e) {
+						e.printStackTrace();
+						doDelete();
+					}
+					myAgent.addBehaviour(new Atendimento());
+				}
+			});
+		}
+		else {
+			System.out.println("Não há médicos disponíveis.");
+			doDelete();
+		}
+		
+
+	}
+	
+	protected void takeDown() {
+		try {
+			System.out.println("Até a próxima!");
+			DFService.deregister(this);
+		} catch(FIPAException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class Atendimento extends Behaviour{
+		private AID melhorMedico;
+		private int melhorPreco;
+		private int repliesCnt = 0;
+		private MessageTemplate mt;
+		private int step = 0;
+
+		@Override
+		public void action() {
+			switch (step) {
+				case 0:
+					ACLMessage cfp = new ACLMessage(ACLMessage.CFP);
+					for (int i = 0; i < AgentesMedicos.length; i++) {
+						cfp.addReceiver(AgentesMedicos[i]);
+					}
+					cfp.setContent(areaDeAtendimento);
+					cfp.setConversationId("atendimento");
+					cfp.setReplyWith("cfp" + System.currentTimeMillis());
+					myAgent.send(cfp);
+					//mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Central de atendimento médico"),
+					//		MessageTemplate.MatchReplyTo(cfp.getReplyWith()));
+					step = 1;
+					break;
+				case 1:
+					ACLMessage reply = myAgent.receive(mt);
+					if (reply != null) {
+						if (reply.getPerformative() == ACLMessage.PROPOSE) {
+							int preco = Integer.parseInt(reply.getContent());
+							if (melhorMedico == null || preco < melhorPreco) {
+								melhorPreco = preco;
+								melhorMedico = reply.getSender();
+							}
+						}
+						repliesCnt++;
+						if (repliesCnt >= AgentesMedicos.length) {
+							step = 2;
+						}
+					}
+					else{
+						block();
+					}
+					break;
+				case 2:
+					ACLMessage atendimento = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+					atendimento.addReceiver(melhorMedico);
+					atendimento.setContent(areaDeAtendimento);
+					atendimento.setConversationId("atendimento");
+					atendimento.setReplyWith("Atendimento "+System.currentTimeMillis());
+					myAgent.send(atendimento);
+					//mt = MessageTemplate.and(MessageTemplate.MatchConversationId("Central de atendimento médico"),
+					//		MessageTemplate.MatchReplyTo(atendimento.getReplyWith()));
+					step = 3;
+					break;
+				case 3:
+					reply = myAgent.receive(mt);
+					if(reply!=null){
+						if(reply.getPerformative() == ACLMessage.INFORM){
+							System.out.println("Atendimento em "+areaDeAtendimento+" com doutor "+reply.getSender().getName());
+							System.out.println("Preco: R$ "+melhorPreco);
+							myAgent.doDelete();
+						}
+						else{
+							System.out.println("Descuple mas não há mais atendimento disponível.");
+						}
+						step = 4;
+					}
+					else{
+						block();
+					}
+					break;
+			}
+		}
+
+		@Override
+		public boolean done() {
+			if(step==2 && melhorMedico == null){
+				System.out.println("Descuple mas o atendimento médico em "+areaDeAtendimento+" com o doutor"+melhorMedico.getName()+" não está mais disponível.");
+			}
+			return ((step == 2 && melhorMedico == null) || step == 4);
+		}
+	}
+}

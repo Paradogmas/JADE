3a8e28285bf6b35a7efce0089670f78949a15a2a
Hiroshi <lucas.hh@hotmail.com>
2019-11-25 04:48:47 -0300

    adicionando vocabulario de medico e paciente

--- src/ontology/HospitalVocabulary.java
+++ src/ontology/HospitalVocabulary.java
@@ -1,5 +1,14 @@
 package ontology;
 
 public interface HospitalVocabulary {
-    
+    // MEDICO
+    public static final String MEDICO = "medico";
+    public static final String MEDICO_NOME = "nome";
+    public static final String MEDICO_CRM = "CRM";
+    public static final String MEDICO_ESPECIALIDADE = "especialidade";
+
+    // PACIENTE
+    public static final String PACIENTE = "paciente";
+    public static final String PACIENTE_NOME = "nome";
+    public static final String PACIENTE_CPF = "CPF";
 }

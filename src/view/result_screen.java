576dc793d6f616ba340da7c2ad4db77e5a1135b0
Lucas362 <lucasvitorifg@gmail.com>
2019-11-25 23:25:29 -0300

    Adding result screen

--- src/view/result_screen.java
+++ src/view/result_screen.java
@@ -1,10 +1,27 @@
 package view;
 
 import javax.swing.*;
+import java.awt.Dimension;
 
 public class result_screen {
     private JPanel result_frame;
     private JLabel paciente_label;
     private JLabel nome_paciente_label;
     private JLabel result_label;
+    private JLabel medico_label;
+    private JLabel valor_label;
+    private JFrame tela;
+
+    public result_screen(String paciente, Integer preco, String especialidadeMedica, String medico){
+        tela = new JFrame("Atendimento médico");
+        tela.setLocation(650, 450);
+        tela.setContentPane(result_frame);
+        tela.pack();
+        tela.setVisible(true);
+        nome_paciente_label.setText("     "+paciente);
+        medico_label.setText("     Medico: " + medico);
+        valor_label.setText("     Valor: " + preco);
+        result_frame = new JPanel();
+        tela.setPreferredSize(new Dimension(640, 480));
+    }
 }
